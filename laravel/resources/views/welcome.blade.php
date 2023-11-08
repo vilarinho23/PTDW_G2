12acce622b55e57c4ea518402b62a035776f6531
vilarinho23 <davidvilarinho@ua.pt>
2023-11-08 18:55:42 +0000

    Criação Estrutura e Página Inicial e idp

---
+++ laravel/resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/welcome.css') }}">
+
+    <title>Início</title>
+</head>
+<body>
+    <header id="header-container">
+        <h1 id="header-titulo">INÍCIO</h1>
+    </header>
+
+    <div id="container">
+        <p id="texto-mensagem">Bem-Vindo ao Gestor de Restrições dos Docentes da ESTGA</p>
+        <p id="texto-opcao">Faça login como:</p>
+        <button id="botao-docente">Docente</button>
+        <button id="botao-comicao">Comissão</button>
+    </div>
+</body>
+</html>

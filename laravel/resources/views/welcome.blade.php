faa77678b4be4227444497bcf365ebebad72e60c
vilarinho23 <davidvilarinho@ua.pt>
2023-11-09 16:59:31 +0000

    atualiza página principal para bootstrap

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -5,20 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/welcome.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
     <title>Início</title>
 </head>
 <body>
-    <header id="header-container">
-        <h1 id="header-titulo">INÍCIO</h1>
-    </header>
+    <nav class="navbar w-100 position-relative" style="background-color: #0EB4BD; height: 60px;">
+        <p class="navbar-brand m-0 p-0 position-absolute top-50 start-50 translate-middle" style="color: black; font-family: 'Inter', sans-serif;">Docente</p>
+    </nav>
 
-    <div id="container">
-        <p id="texto-mensagem">Bem-Vindo ao Gestor de Restrições dos Docentes da ESTGA</p>
-        <p id="texto-opcao">Faça login como:</p>
-        <button id="botao-docente">Docente</button>
-        <button id="botao-comicao">Comissão</button>
+    <div class="container p-0 d-flex flex-column align-items-center mx-auto" style="width: fit-content; margin-top: 200px;">
+        <p class="" style="font-size: 20px; font-weight: 600;">Bem-Vindo ao Gestor de Restrições dos Docentes da ESTGA</p>
+        <p class="mt-3" style="color: rgb(26, 26, 26)">Faça login como:</p>
+        <div class="mt-4">
+            <button class="button-style me-4" style="width: 150px; height: 40px; font-size: 20px">Docente</button>
+            <button class="button-style" style="width: 150px; height: 40px; font-size: 20px">Comissão</button>
+        </div>
     </div>
 </body>
 </html>

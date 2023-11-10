a827418cc2057ac90016e18b083eecbe1b256e3e
Gustavo <gustavo.silva9@ua.pt>
2023-11-10 02:01:15 +0000

    Página Docente Sem UC Atribuidas

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -28,3 +28,7 @@ Route::get('/docente', function(){
 Route::get('/restricoes',function(){
     return view('restricoes');
 });
+
+Route::get('/docenteSemUC',function(){
+    return view('docenteSemUCAtribuidas');
+});

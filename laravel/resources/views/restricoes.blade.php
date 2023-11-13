7cd27dab30b03147dd05e24628d9949119713d51
João Rodrigues <jpprodrigues@ua.pt>
2023-11-13 01:43:18 +0000

    correcao de erros na pagina de submissoes e restricoes

--- laravel/resources/views/restricoes.blade.php
+++ laravel/resources/views/restricoes.blade.php
@@ -1,15 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-    @include('partials._head',["titulo"=>"Restrições"])
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
-    <title>Restrições</title>
-</head>
+    @include('partials._head',["titulo"=>"Restriçoes"])
 <body>
     @include('partials._headerDocente')
     <div class="d-flex align-items-center justify-content-center">

508fdc111c6e91e96aa33fcdf2c495603a4300c9
João Rodrigues <jpprodrigues@ua.pt>
2023-11-14 01:21:52 +0000

    adicao da navegacao entre paginas

--- laravel/resources/views/partials/_headerDocente.blade.php
+++ laravel/resources/views/partials/_headerDocente.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar w-100 position-relative" style="background-color: #0EB4BD; height: 60px;">
-  <p class="navbar-brand m-0 p-0 position-absolute top-50 start-50 translate-middle" style="color: black; font-family: 'Inter', sans-serif;">Docente</p>
+  <a class="navbar-brand m-0 p-0 position-absolute top-50 start-50 translate-middle" style="color: black; font-family: 'Inter', sans-serif;" href="{{url("docente")}}">Docente</a>
   <div class="position-absolute end-0 d-flex align-items-center">
       <p class="m-0 p-0 me-3" style="color: black; font-family: 'Inter', sans-serif;">Pedro Antunes</p>
       <img class="m-0 p-0 me-4" src="{{ asset('images/logout.png') }}" onmouseover="this.style.cursor='pointer';" onmouseout="this.style.cursor='default';" alt="Image_logout" data-bs-toggle="modal" data-bs-target="#logOutModal">
@@ -13,7 +13,7 @@
         <h5 class="modal-title mx-auto" id="exampleModalLabel">Pretende sair da sua conta?</h5>
       </div>
       <div class="modal-footer d-flex justify-content-center border-0">
-        <button type="button" class="mx-2 button-style" style="width: 130px; height: 30px;">Sair</button>
+        <button type="button" class="mx-2 button-style" style="width: 130px; height: 30px;" id="logOut-button">Sair</button>
         <button type="button" class="mx-2 button-style" style="width: 130px; height: 30px;" data-bs-dismiss="modal">Cancelar</button>
       </div>
     </div>
--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -11,8 +11,8 @@
         <p class="" style="font-size: 20px; font-weight: 600;">Bem-Vindo ao Gestor de Restrições dos Docentes da ESTGA</p>
         <p class="mt-3" style="color: rgb(26, 26, 26)">Faça login como:</p>
         <div class="mt-4">
-            <button class="button-style me-4" style="width: 150px; height: 40px; font-size: 20px">Docente</button>
-            <button class="button-style" style="width: 150px; height: 40px; font-size: 20px">Comissão</button>
+            <button class="button-style me-4" style="width: 150px; height: 40px; font-size: 20px" id="docente-button">Docente</button>
+            <button class="button-style" style="width: 150px; height: 40px; font-size: 20px" id="comissao-button">Comissão</button>
         </div>
     </div>
 </body>

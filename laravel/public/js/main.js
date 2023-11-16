84b5256c92057a5a5e45f031d56f0458e7eefa3e
Márcio Tavares <marciotavares@ua.pt>
2023-11-16 23:58:11 +0000

    Correção das ligações a rotas via JS
    Pequena correção na rota atribuicaouc

--- laravel/public/js/main.js
+++ laravel/public/js/main.js
@@ -1,19 +1,21 @@
+// Caminhos para as páginas definidos no Blade
+
 const botaoDocente = document.getElementById("docente-button");
 botaoDocente?.addEventListener('click', () => {
-    window.location.href = '/docente'
+    window.location.href = docenteUrl;
 })
 
 const botaoComissao = document.getElementById("comissao-button");
 botaoComissao?.addEventListener('click', () => {
-    window.location.href = '/submissoes'
+    window.location.href = comissaoUrl;
 })
 
 const botaoLogOut = document.getElementById("logOut-button");
 botaoLogOut?.addEventListener('click', () => {
-    window.location.href = '/'
+    window.location.href = logOutUrl;
 })
 
 const botaoPreencher = document.getElementById("preencher-button");
 botaoPreencher?.addEventListener('click', () => {
-    window.location.href = '/restricoes'
+    window.location.href = preencherUrl;
 })

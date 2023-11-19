d07224048490c6eb5e5eb790e228d1c8a295ac47
Márcio Tavares <marciotavares@ua.pt>
2023-11-19 16:02:50 +0000

    Gestor de Docentes - correções validação W3C

--- laravel/resources/views/gestorDocentes.blade.php
+++ laravel/resources/views/gestorDocentes.blade.php
@@ -10,8 +10,7 @@
             <div class="d-flex justify-content-between">
                 <div class="d-flex align-items-center gap-2">
                     <div class="input-group rounded">
-                        <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search"
-                            aria-describedby="search-addon" />
+                        <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search">
                     </div>
                     <div>
                         <img src="images/search-interface-symbol.png" alt="search">
@@ -40,21 +39,21 @@
                                 <th scope="row">1</th>
                                 <td>Paulo Rui Santos</td>
                                 <td>Tecnologias Aplicadas ao Trabalho</td>
-                                <td><img src="images\edit.png" alt="edit" data-bs-toggle="modal"
+                                <td><img src="images/edit.png" alt="edit" data-bs-toggle="modal"
                                         data-bs-target="#editarModal"></td>
                             </tr>
                             <tr>
                                 <th scope="row">2</th>
                                 <td>Paulo Rui Santos</td>
                                 <td>Tecnologias Aplicadas ao Trabalho</td>
-                                <td><img src="images\edit.png" alt="edit" data-bs-toggle="modal"
+                                <td><img src="images/edit.png" alt="edit" data-bs-toggle="modal"
                                         data-bs-target="#editarModal"></td>
                             </tr>
                             <tr>
                                 <th scope="row">3</th>
                                 <td>Paulo Rui Santos</td>
                                 <td>Tecnologias Aplicadas ao Trabalho</td>
-                                <td><img src="images\edit.png" alt="edit" data-bs-toggle="modal"
+                                <td><img src="images/edit.png" alt="edit" data-bs-toggle="modal"
                                         data-bs-target="#editarModal"></td>
                             </tr>
                         </tbody>
@@ -79,31 +78,31 @@
                 </div>
 
                 <div class="modal-body">
-                    <form method="POST" action="">
+                    <form method="POST" action="/">
                         @csrf
                         <div class="container-fluid">
                             <div class="row g-3 align-items-center">
                                 <div class="col-sm-2">
-                                    <label for="inputNFuncionario" class="col-form-label">Nº funcionário</label>
+                                    <label for="inputAdicionarNFuncionario" class="col-form-label">Nº funcionário</label>
                                 </div>
                                 <div class="col-sm">
-                                    <input type="text" class="form-control" id="inputNFuncionario" placeholder="">
+                                    <input type="text" class="form-control" id="inputAdicionarNFuncionario" placeholder="">
                                 </div>
                             </div>
                             <div class="row mt-2 g-3 align-items-center">
                                 <div class="col-sm-2">
-                                    <label for="inputNomeDocente" class="col-form-label">Nome docente</label>
+                                    <label for="inputAdicionarNome" class="col-form-label">Nome docente</label>
                                 </div>
                                 <div class="col">
-                                    <input type="text" class="form-control" id="inputNomeDocente" placeholder="">
+                                    <input type="text" class="form-control" id="inputAdicionarNome" placeholder="">
                                 </div>
                             </div>
                             <div class="row mt-2 g-3 align-items-center">
                                 <div class="col-sm-2">
-                                    <label for="inputACNDocente" class="col-form-label">ACN docente</label>
+                                    <label for="inputAdicionarAcn" class="col-form-label">ACN docente</label>
                                 </div>
                                 <div class="col">
-                                    <input type="text" class="form-control" id="inputACNDocente" placeholder="">
+                                    <input type="text" class="form-control" id="inputAdicionarAcn" placeholder="">
                                 </div>
                             </div>
                         </div>
@@ -129,31 +128,31 @@
                 </div>
 
                 <div class="modal-body">
-                    <form method="POST" action="">
+                    <form method="POST" action="/">
                         @csrf
                         <div class="container-fluid">
                             <div class="row g-3 align-items-center">
                                 <div class="col-sm-2">
-                                    <label for="inputNFuncionario" class="col-form-label">Nº funcionário</label>
+                                    <label for="inputEditarNFuncionario" class="col-form-label">Nº funcionário</label>
                                 </div>
                                 <div class="col-sm">
-                                    <input type="text" class="form-control" id="inputNFuncionario" placeholder="">
+                                    <input type="text" class="form-control" id="inputEditarNFuncionario" placeholder="">
                                 </div>
                             </div>
                             <div class="row mt-2 g-3 align-items-center">
                                 <div class="col-sm-2">
-                                    <label for="inputNomeDocente" class="col-form-label">Nome docente</label>
+                                    <label for="inputEditarNome" class="col-form-label">Nome docente</label>
                                 </div>
                                 <div class="col">
-                                    <input type="text" class="form-control" id="inputNomeDocente" placeholder="">
+                                    <input type="text" class="form-control" id="inputEditarNome" placeholder="">
                                 </div>
                             </div>
                             <div class="row mt-2 g-3 align-items-center">
                                 <div class="col-sm-2">
-                                    <label for="inputACNDocente" class="col-form-label">ACN docente</label>
+                                    <label for="inputEditarAcn" class="col-form-label">ACN docente</label>
                                 </div>
                                 <div class="col">
-                                    <input type="text" class="form-control" id="inputACNDocente" placeholder="">
+                                    <input type="text" class="form-control" id="inputEditarAcn" placeholder="">
                                 </div>
                             </div>
                         </div>

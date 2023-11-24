f572907929fa9d2d9d1bd88dac6a232ccc731702
Márcio Tavares <marciotavares@ua.pt>
2023-11-24 18:52:28 +0000

    Controller para Submissoes
    funcao docente() para a página do docente

---
+++ laravel/app/Http/Controllers/SubmissaoController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Docente;
+use App\Models\KeyValue;
+
+class SubmissaoController extends Controller
+{
+    public function docente()
+    {
+        // Docente
+        $docenteNrFunc = 2;
+        $docente = Docente::find($docenteNrFunc);
+        if ($docente == null) abort(404);
+
+        // Unidades Curriculares
+        $ucs = $docente->unidadesCurriculares;
+        $respUCs = $docente->respUnidadesCurriculares;
+
+        $ucs = $ucs->merge($respUCs);
+        foreach ($ucs as $uc) $uc->responsavel = $respUCs->contains($uc);
+
+        // Restrições, data de submissao e nome
+        $restricoes = $docente->restricoes;
+        $dataSubmissao = $docente->data_submissao;
+        $nomeDocente = $docente->nome_docente;
+
+        // Obter data de conclusao
+        $dataConclusao = KeyValue::val('data_conclusao');
+
+
+        // Retornar docente
+        return response()->json([
+            'nomeDocente' => $nomeDocente,
+            'ucs' => $ucs,
+            'restricoes' => $restricoes,
+            'dataSubmissao' => $dataSubmissao,
+            'dataConclusao' => $dataConclusao
+        ]);
+    }
+}
---
+++ laravel/database/seeders/DocenteSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Docente;
+use App\Models\UnidadeCurricular;
+
+class DocenteSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Docente sem UCs
+        $docenteSemUCs = Docente::find(1);
+        if ($docenteSemUCs == null)
+        {
+            $docenteSemUCs = Docente::factory()->make();
+            $docenteSemUCs->num_func = 1;
+            $docenteSemUCs->save();
+        }
+
+        // Docente com UCs
+        $docenteComUCs = Docente::find(2);
+        if ($docenteComUCs == null)
+        {
+            $docenteComUCs = Docente::factory()->make();
+            $docenteComUCs->num_func = 2;
+            $docenteComUCs->save();
+        }
+
+        if ($docenteComUCs->unidadesCurriculares->count() == 0)
+        {
+            $ucs = UnidadeCurricular::all();
+            $ucsDocente = fake()->randomElements($ucs, 2);
+            foreach ($ucsDocente as $uc) $docenteComUCs->unidadesCurriculares()->attach($uc->id);
+        }
+    }
+}

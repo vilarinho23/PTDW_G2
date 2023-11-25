f912a9f66592f9582c64c006088e04c942c76547
Márcio Tavares <66422486+MarcioTROLL@users.noreply.github.com>
2023-11-25 18:42:14 +0000

    Revert "Correção de docente sem ucs depois de atribuitas"

--- laravel/database/seeders/DocenteSeeder.php
+++ laravel/database/seeders/DocenteSeeder.php
@@ -40,13 +40,10 @@ class DocenteSeeder extends Seeder
                     $docenteComUCs->num_func,
                     ['perc_horas' => fake()->numberBetween(0, 100)]
                 );
-            $docenteComUCs = Docente::find(2);
         }
 
         if ($docenteComUCs->respUnidadesCurriculares->isEmpty())
         {
-            if ($docenteComUCs->unidadesCurriculares->isEmpty()) return;
-
             $primeiraUC = $docenteComUCs->unidadesCurriculares[0];
             $primeiraUC->responsavel()->associate($docenteComUCs);
             $primeiraUC->save();

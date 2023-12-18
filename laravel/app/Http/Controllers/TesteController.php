664b70b983b8a06829a48904f20c1d1a0563f976
Márcio Tavares <marciotavares@ua.pt>
2023-12-18 23:08:36 +0000

    Last Import info na base de dados (testar/import)
    KeyValue no testar/models

--- laravel/app/Http/Controllers/TesteController.php
+++ laravel/app/Http/Controllers/TesteController.php
@@ -5,9 +5,11 @@ namespace App\Http\Controllers;
 use App\Imports\DSDImport;
 use App\Models\Curso;
 use App\Models\Docente;
+use App\Models\KeyValue;
 use App\Models\Laboratorio;
 use App\Models\RestricaoHorario;
 use App\Models\UnidadeCurricular;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Storage;
 use Maatwebsite\Excel\Facades\Excel;
 
@@ -15,12 +17,13 @@ class TesteController extends Controller
 {
     public function testarModels()
     {
-        // Buscar/criar dados
+        // Buscar dados
         $cursos = Curso::with('unidadesCurriculares')->get();
         $laboratorios = Laboratorio::with('unidadesCurriculares')->get();
         $docentes = Docente::with('restricoes', 'respUnidadesCurriculares', 'unidadesCurriculares')->get();
         $restricoes = RestricaoHorario::with('docente')->get();
         $ucs = UnidadeCurricular::with('cursos', 'laboratorios', 'responsavel', 'docentes')->get();
+        $kvs = KeyValue::all();
 
         // Retornar JSON
         return response()->json([
@@ -28,7 +31,8 @@ class TesteController extends Controller
             'laboratorios' => $laboratorios,
             'docentes' => $docentes,
             'restricoes' => $restricoes,
-            'ucs' => $ucs
+            'ucs' => $ucs,
+            'kvs' => $kvs
         ]);
     }
 
@@ -47,6 +51,17 @@ class TesteController extends Controller
         $import = new DSDImport;
         Excel::import($import, $filename);
 
+        // Set last import filename, uploader and timestamp
+        KeyValue::set('last_import:filename', $filename);
+        KeyValue::set('last_import:uploader', 'TesteController@testarImport');
+        KeyValue::set('last_import:timestamp', Carbon::now()->format('d/m/Y H:i:s'));
+
+        // Set last import errors (line numbers)
+        $errorLines = array_keys($import->getErrors());
+        $errorLines = $errorLines == [] ? null : implode(',', $errorLines);
+        KeyValue::set('last_import:line_errors', $errorLines);
+
+        // Return response
         return response()->json([
             'message' => "Importação do ficheiro $filename concluída",
             'errors' => $import->getErrors()

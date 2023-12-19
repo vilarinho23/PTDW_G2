e99fdc26defc25512951cace4eec3973f11cd4a1
Márcio Tavares <marciotavares@ua.pt>
2023-12-19 00:52:44 +0000

    Abas porUC e porCurso funcionais
    Alterações em RestricoesPorUCSheet e RestricoesPorCursoSheet (conteúdo da aba).

--- laravel/app/Exports/Sheets/RestricoesPorCursoSheet.php
+++ laravel/app/Exports/Sheets/RestricoesPorCursoSheet.php
@@ -29,7 +29,44 @@ class RestricoesPorCursoSheet implements FromArray, WithTitle
             ['n.º Func', 'nome docente', 'cód', 'ACN', 'R', 'nome UC', 'curso', 'h', 'n.º', 'subT']
         ];
 
-        // TODO RestricoesPorCursoSheet content
+        // For each docente
+        foreach ($this->docentes as $docente)
+        {
+            // Docente properties
+            $numFunc = $docente->num_func;
+            $nomeDocente = $docente->nome_docente;
+
+
+            // Unidades Curriculares
+            foreach ($docente->unidadesCurriculares as $uc)
+            {
+                // UC properties
+                $codUC = $uc->cod_uc;
+                $nomeUC = $uc->nome_uc;
+                $horasUC = $uc->horas_uc;
+                $acnUC = $uc->acn_uc;
+                $resp = $uc->num_func_resp == $numFunc ? 'X' : '';
+
+                // Cursos
+                $cursos = $uc->cursos->map(function ($curso) {
+                    return $curso->acron_curso;
+                })->join(',') ?? '';
+
+                // Docente-UC properties
+                $perc = $uc->pivot->perc_horas / 100;
+                $subT = $perc * $horasUC;
+
+
+                // Add to array (export data)
+                $atrib[] = [
+                    $numFunc, $nomeDocente,
+                    $codUC, $acnUC,
+                    $resp, $nomeUC,
+                    $cursos, $horasUC,
+                    $perc, $subT
+                ];
+            }
+        }
 
         return $atrib;
     }
